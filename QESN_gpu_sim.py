862a29183aed7438cfe2c975f9ac5c6573c1f57a
Erik Connerty <13265359+econnerty@users.noreply.github.com>
2025-07-29 08:56:31 -0500

    Update QESN_gpu_sim.py

--- QESN_gpu_sim.py
+++ QESN_gpu_sim.py
@@ -75,9 +75,9 @@ if __name__ == '__main__':
 	
 		#Load in lorenz data
 		#Get data
-		start = 500
-		n_pts = 2000
-		test_pts = 500
+		start = 0
+		n_pts = 6900
+		test_pts = 3000
 		train_input_signal, test_input_signal, train_data, test_data = QRNN.get_lorenz_data(startup=start,n_pts=n_pts,test_pts=test_pts)
 		#train_input_signal = np.load(f'./data/{signal_type}_signal.npy')
 		# #Get circuit
@@ -118,7 +118,9 @@ if __name__ == '__main__':
 		expectation = QRNN.get_probability_matrix(result, register_names, n_qubits//2)
 	
 		np.save(f'./results_server/expectation_values/{signal_type}_prob_values_{n_shots}_shots_{n_qubits}_qubits_rep{repeat_blocks}_{date}_{seed}_{start}_{n_pts}_reptest_sparse_norotationread_cnoto', expectation)
-	
+
+
+		###Run it again on the test dataset and save those results to reproduce the paper
 		f.close()
 
 
